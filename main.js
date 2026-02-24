2ef1d1b5509da12db056800fde425a0f4ca05ab1
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@dsn127m1246732.cfp127aluno.local>
2026-02-24 15:20:35 -0300

    Validação de entradas

--- main.js
+++ main.js
@@ -3,10 +3,17 @@
 function adicionarProduto() {
     const produto = document.getElementById('produto')
     const lista = document.getElementById('lista')
-    
-    const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'bg-blue-200 px-8 py-2 text-center'
 
-    lista.appendChild(span)
+    const span = document.createElement('span')
+
+    if (produto.value == '' || !isNaN(produto.value)) {
+        alert('ERRO: CAMPOS NÃO FORAM PREENCHIDOS CORRETAMENTE!')
+        produto.value = ''
+    } else {
+        span.textContent = produto.value
+        span.className = 'bg-blue-200 px-8 py-2 text-center'
+
+        lista.appendChild(span)
+        produto.value = ''
+    }
 }
