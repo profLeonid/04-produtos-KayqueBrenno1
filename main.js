38f19671fa0d52421201ffdaa28a673202110edd
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@dsn127m1246732.cfp127aluno.local>
2026-02-24 16:48:38 -0300

    Validação das caixas de textos e lista em coluna de três

--- main.js
+++ main.js
@@ -1,19 +1,45 @@
 'use strict'
 
 function adicionarProduto() {
+    const codigo = document.getElementById('codigo')
     const produto = document.getElementById('produto')
+    const quantidade = document.getElementById('quantidade')
     const lista = document.getElementById('lista')
 
-    const span = document.createElement('span')
+    const spanCodigo = document.createElement('span')
+    const spanProduto = document.createElement('span')
+    const spanQuantidade = document.createElement('span')
+    const div = document.createElement('div')
 
-    if (produto.value == '' || !isNaN(produto.value)) {
-        alert('ERRO: CAMPOS NÃO FORAM PREENCHIDOS CORRETAMENTE!')
+    if (codigo.value == '' || isNaN(codigo.value)) {
+        alert('ERRO: VEJA SE O CÓDIGO FOI PREENCHIDO CORRETAMENTE')
+        codigo.value = ''
+    } else if (produto.value == '' || !isNaN(produto.value)) {
+        alert('ERRO: VEJA SE O PRODUTO FOI PREENCHIDO CORRETAMENTE')
         produto.value = ''
+    } else if (quantidade.value == '' || isNaN(quantidade.value)) {
+        alert('ERRO: VEJA SE A QUANTIDADE FOI PREENCHIDA CORRETAMENTE')
+        quantidade.value = ''
     } else {
-        span.textContent = produto.value
-        span.className = 'bg-blue-200 px-8 py-2 text-center'
+        div.className = 'flex justify-center gap-2 w-full'
 
-        lista.appendChild(span)
+        spanCodigo.textContent = codigo.value
+        spanCodigo.className = 'bg-purple-200 px-8 py-2 text-center'
+
+        spanProduto.textContent = produto.value
+        spanProduto.className = 'bg-purple-200 px-8 py-2 text-center'
+
+        spanQuantidade.textContent = quantidade.value
+        spanQuantidade.className = 'bg-purple-200 px-8 py-2 text-center'
+
+        div.appendChild(spanCodigo)
+        div.appendChild(spanProduto)
+        div.appendChild(spanQuantidade)
+        lista.appendChild(div)
+        
+
+        codigo.value = ''
         produto.value = ''
+        quantidade.value = ''
     }
 }
